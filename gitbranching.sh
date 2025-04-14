83e67eab052e9b6bfdea8c11c14396ef81ef7e54
putomarcin <165215804+putomarcin@users.noreply.github.com>
2025-04-14 22:02:10 +0200

    Add files via upload

---
+++ gitbranching.sh
@@ -0,0 +1,20 @@
+git rebase <nazwa>
+^
+~<num>
+git reset <nazwa>~<num>
+git revert <nazwa>
+git cherry-pick <Commit1> <Commit2> <...>
+git rebase -i <nazwa>
+git commit --amend
+git tag <nazwa tagu> <nazwa>
+git describe <ref>
+wygląd polecenia : <tag>_<numCommits>_g<hash
+git clone
+/o
+git fetch
+git pull
+git fakeTeamwork
+git push
+git pull --rebase
+foo
+git push origin <nazwa>
